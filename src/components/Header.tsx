e01c2fed658c657dbee4918f381e5cb3b9c3dce5
Alix Bell <x.bavayra@gmail.com>
2025-12-26 18:13:41 +0100

    fix: fixed header position so it will not cause issues with side bar and for better ux

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="bg-primary-200 shadow-md sticky top-0 z-50">
+    <header className="bg-primary-200 shadow-md fixed top-0 z-50">
       <div className="container mx-auto px-4 py-2">
         <div className="flex items-center justify-between">
           <img
