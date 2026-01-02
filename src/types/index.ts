64a9975a0ac39904023ba7ab9b4152f837066a6d
Alix Bell <x.bavayra@gmail.com>
2026-01-02 15:29:05 +0100

    fix: improved types in Dog and dogFilters interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,19 +2,19 @@ export interface Dog {
   id: number;
   name: string;
   breed: string;
-  age: string; // 'Young' | 'Adult' | 'Senior'
+  age: '' | 'under-1' | '1-3' | '4-7' | '8-plus';
   gender: 'Boy' | 'Girl';
-  health: string; // 'Healthy' | 'Special needs'
+  health: 'Healthy' | 'Special needs';
   description: string;
   imageUrl: string;
   isAdopted: boolean;
 }
 
 export interface DogFilters {
-  age: string; // 'Young' | 'Adult' | 'Senior' | ''
-  gender: string; // 'Boy' | 'Girl' | ''
-  health: string; // 'Healthy' | 'Special needs' | ''
-  breed: string; // 'Rottweiler' | 'Cane Corso' | 'Mixed breed' | ''
+  age: '' | 'under-1' | '1-3' | '4-7' | '8-plus';
+  gender: 'Boy' | 'Girl' | '';
+  health: '' | 'Healthy' | 'Special needs';
+  breed: '' | 'Rottweiler' | 'Cane Corso' | 'Mixed breed';
 }
 
 export interface GalleryImage {
