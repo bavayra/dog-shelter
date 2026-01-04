5db2c2c96ec788d0851a0300342d8bc90ad106e5
Alix Bell <x.bavayra@gmail.com>
2026-01-04 18:23:56 +0100

    chore: deleted unused aliases

--- vite.config.ts
+++ vite.config.ts
@@ -19,13 +19,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
   build: {
