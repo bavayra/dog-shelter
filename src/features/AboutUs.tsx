2968657f75c74e7888b3f3de777305484cdf8599
Alix Bell <x.bavayra@gmail.com>
2026-01-08 13:50:54 +0100

    style: restyled AboutUs section: fonts, colors, margins and paddings

--- src/features/AboutUs.tsx
+++ src/features/AboutUs.tsx
@@ -2,26 +2,24 @@ import PhotoCarousel from './PhotoCarousel';
 
 const AboutUs = () => {
   return (
-    <section id="about-us" className="bg-primary-50 py-10">
+    <section id="about-us" className="bg-primary-50 py-8">
       <div className="container mx-auto px-4">
-        <h2 className="text-primary-500 3xs:text-4xl mb-8 text-center text-5xl font-bold">
-          About Us
-        </h2>
+        <h2 className="typography-h2 mb-4 text-center">About Us</h2>
 
-        <div className="bg-primary-300 text-primary-50 relative z-10 mx-auto my-7 max-w-xl rounded-2xl px-0 px-3 py-6 text-center text-lg">
-          <p className="mb-2">
+        <div className="bg-primary-100 typography-body border-primary-300 relative z-10 mx-auto my-2 max-w-xl rounded-2xl border-2 px-3 py-6 text-center">
+          <p>
             Rotvodom was founded in 2010 by Rotvomama, who started building a
             shelter in her own backyard by herself. She started taking in one
             dog after another, relying solely on herself and her few friends.
           </p>
-          <hr className="border-primary-100 my-4 border-t" />
-          <p className="mb-2">
+          <hr className="border-primary-500 my-4 border-t" />
+          <p>
             Soon enough the shelter grew, and today we are a team of dedicated
             volunteers and professionals who work tirelessly to rescue,
             rehabilitate, and rehome dogs in need. Over the years, we have saved
             hundreds of dogs from dire situations.
           </p>
-          <hr className="border-primary-100 my-4 border-t" />
+          <hr className="border-primary-500 my-4 border-t" />
           <p>
             Our mission is to give every dog a second chance at life and to find
             them loving forever homes. We believe that every dog deserves a
@@ -30,7 +28,7 @@ const AboutUs = () => {
           </p>
         </div>
 
-        <h3 className="text-primary-500 mb-8 text-center text-3xl font-semibold">
+        <h3 className="typography-h3 text-primary-500 my-6 text-center">
           Moments from Rotvodom
         </h3>
         <PhotoCarousel />
