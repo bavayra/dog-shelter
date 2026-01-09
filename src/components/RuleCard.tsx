5d6b3fefbc1ba7222fc5a8465d2b9e70b89873fd
Alix Bell <x.bavayra@gmail.com>
2026-01-09 15:24:49 +0100

    style: updated the AboutUs section style for >425 such as margins, font sizes and paddings

--- src/components/RuleCard.tsx
+++ src/components/RuleCard.tsx
@@ -5,7 +5,7 @@ interface RuleCardProps {
 
 const RuleCard = ({ number, text }: RuleCardProps) => {
   return (
-    <div className="mx-2 rounded-lg bg-white px-6 py-4 shadow-md">
+    <div className="mx-2 rounded-lg bg-white px-6 py-4 shadow-md sm:mx-8">
       <div className="flex items-start gap-2">
         <span className="text-primary-500 bg-primary-100 -ml-3 flex h-6 w-6 shrink-0 items-start justify-center rounded-full font-bold">
           {number}
