538661516b15e16a86fcdaa2f2013d6b1758e1e0
Alix Bell <x.bavayra@gmail.com>
2026-01-18 17:13:58 +0100

    style(AdoptionRules): corrected margins and paddings. Decreased font size of the rules cards text and number icons. Decreased btn's text sizes and paddings

--- src/components/RuleCard.tsx
+++ src/components/RuleCard.tsx
@@ -7,10 +7,12 @@ const RuleCard = ({ number, text }: RuleCardProps) => {
   return (
     <div className="xs:mx-6 mx-2 flex h-full justify-center rounded-lg bg-white px-6 py-4 text-center shadow-md sm:mx-8 md:mx-16 lg:mx-4">
       <div className="flex items-center gap-2">
-        <span className="text-primary-500 bg-primary-100 -ml-3 flex h-6 w-6 shrink-0 items-center justify-center rounded-full font-bold lg:h-8 lg:w-8">
+        <span className="text-primary-500 bg-primary-100 -ml-3 flex h-5 w-5 shrink-0 items-center justify-center rounded-full font-bold lg:h-8 lg:w-8">
           {number}
         </span>
-        <p className="typography-body md:text-xl lg:px-2 lg:text-lg">{text}</p>
+        <p className="typography-small font-normal md:text-xl lg:px-2 lg:text-lg">
+          {text}
+        </p>
       </div>
     </div>
   );
