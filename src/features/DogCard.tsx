3651437c31031c8b20d39f2a4da1a72c21da9281
Alix Bell <x.bavayra@gmail.com>
2026-01-19 19:13:16 +0100

    style(dogCard): increased font size for the description

--- src/features/DogCard.tsx
+++ src/features/DogCard.tsx
@@ -85,7 +85,7 @@ const DogCard = memo((dog: Dog) => {
             </div>
           )}
           <div className="min-h-0 flex-1">
-            <p className="typography-small mb-4 line-clamp-5 sm:text-base lg:mb-2">
+            <p className="typography-small mb-4 line-clamp-5 text-sm sm:text-base lg:mb-2">
               {description}
             </p>
           </div>
