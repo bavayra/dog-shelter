d8d90f826f6585c3e0a158728711777096f20f0f
Alix Bell <x.bavayra@gmail.com>
2026-01-20 17:52:37 +0100

    style(header): increased sizes for the home icon, logo and h1 for 412+

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,21 +10,18 @@ const Header = () => {
 
   return (
     <header className="bg-primary-200 sticky top-0 z-50 w-full overflow-x-hidden shadow-md">
-      <div className="container mx-auto px-2 py-1 md:px-4">
+      <div className="xs:py-2 container mx-auto px-2 py-1 md:px-4">
         <div className="flex flex-row items-center justify-evenly gap-30">
           <div className="flex min-w-0 flex-row items-center gap-1 sm:gap-2">
             <img
               src={LogoIcon}
               alt="Rotvodom Dog Shelter Logo"
-              className="border-primary-500 h-8 w-8 shrink-0 rounded-full border-2 bg-transparent object-center sm:h-10 sm:w-10 md:h-14 md:w-14"
+              className="border-primary-500 xs:w-10 xs:h-10 h-8 w-8 shrink-0 rounded-full border-2 bg-transparent object-center sm:h-12 sm:w-12 md:h-14 md:w-14"
             />
             <div className="flex min-w-0 items-center">
-              <h1 className="text-primary-500 min-w-0 truncate text-sm leading-tight font-bold sm:text-base md:text-xl xl:text-2xl">
+              <h1 className="text-primary-500 xs:text-base min-w-0 truncate text-sm leading-tight font-bold sm:text-2xl md:text-xl xl:text-2xl">
                 {APP_NAME}
               </h1>
-              <p className="text-primary-500 hidden text-xl font-semibold md:ml-2 md:flex lg:hidden">
-                - Best Friends' Shelter
-              </p>
             </div>
           </div>
 
@@ -64,7 +61,7 @@ const Header = () => {
             aria-label="Go to home section"
           >
             <HomeIcon
-              className="hover:text-primary-700 text-primary-500 h-5 w-5 shrink-0 cursor-pointer object-contain transition hover:scale-115 sm:h-6 sm:w-6"
+              className="hover:text-primary-700 text-primary-500 xs:w-7 xs:h-7 h-5 w-5 shrink-0 cursor-pointer object-contain transition hover:scale-115 sm:h-8 sm:w-8"
               aria-label="Home"
             />
           </a>
