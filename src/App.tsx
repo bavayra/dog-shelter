acb4a8c99eee0d6d1ddbd7776c2ca28ec40841b0
Alix Bell <x.bavayra@gmail.com>
2026-01-21 15:15:06 +0100

    style(hero-768): decreased max-h of the section, increased dog's pic and decor circles sizes, positioned them correctly with translate, increased font-size for the title, dog stats and text  block, corrected margins and paddings

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,24 @@ function App() {
         <main className="flex-1">
           <section
             id="hero"
-            className="from-primary-300 to-primary-50 border-primary-500 xs:min-h-[calc(100vh-4rem)] tablet-sm:min-h-[calc(100vh-4rem)] relative flex min-h-[calc(100vh-2rem)] flex-col overflow-hidden border-b-3 bg-linear-to-b px-4 pt-6 pb-6 shadow-md min-[425px]:min-h-[calc(100vh-5rem)] md:pt-10 md:pb-10 lg:min-h-[calc(100vh-72px)]"
+            className="from-primary-300 to-primary-50 border-primary-500 xs:min-h-[calc(100vh-4rem)] tablet-sm:min-h-[calc(100vh-4rem)] relative flex min-h-[calc(100vh-2rem)] flex-col overflow-hidden border-b-3 bg-linear-to-b px-4 pt-6 pb-6 shadow-md min-[425px]:min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-8rem)] md:pt-10 md:pb-10 lg:min-h-[calc(100vh-72px)]"
           >
-            <h2 className="text-primary-700 xs:text-4xl xs:mt-16 tablet-sm:px-4 tablet-sm:mt-26 relative z-10 mx-auto mt-10 w-full text-center text-3xl font-semibold sm:text-5xl md:mt-8 lg:text-6xl">
+            <h2 className="text-primary-700 xs:text-4xl xs:mt-16 tablet-sm:px-4 tablet-sm:mt-26 relative z-10 mx-auto mt-10 w-full text-center text-3xl font-semibold sm:text-5xl md:mt-10 md:text-6xl lg:text-6xl">
               Give a Shelter Dog a Loving Home
             </h2>
             <DecorativeCircle
               size={40}
               xsSize={52}
               tabletSmSize={60}
-              mdSize={56}
+              mdSize={72}
               lgSize={64}
               color="primary-500"
               top="0"
               left="0"
               opacity={70}
-              className="tablet-sm:translate-y-full -translate-x-1/2 translate-y-[80%] md:top-6 lg:top-8"
+              className="tablet-sm:translate-y-full -translate-x-1/2 translate-y-[80%] md:translate-y-2/3 lg:top-8"
             />
-            <div className="glass-card text-primary-700 xs:text-base xs:mt-8 tablet-sm:mt-12 tablet-sm:text-xl tablet-sm:px-8 relative z-10 mx-auto mt-6 max-w-xl rounded-3xl p-4 text-center text-xs font-semibold min-[425px]:mt-4 sm:text-lg md:mt-6 md:text-lg">
+            <div className="glass-card text-primary-700 xs:text-base xs:mt-8 tablet-sm:mt-12 tablet-sm:text-xl tablet-sm:px-8 relative z-10 mx-auto mt-6 max-w-xl rounded-3xl p-4 text-center text-xs font-semibold min-[425px]:mt-4 sm:text-lg md:mt-10">
               <p className="mb-2">
                 DOGS IN ROTVODOM NOW: {shelterStats.totalDogsNow}{' '}
               </p>
@@ -51,7 +51,7 @@ function App() {
               <p>TOTAL DOGS ADOPTED: {shelterStats.dogsAdoptedTotal}</p>
             </div>
 
-            <div className="-mt-2 flex justify-center md:mt-6">
+            <div className="-mt-2 flex justify-center">
               <Button
                 variant="primary"
                 size="medium"
@@ -66,8 +66,8 @@ function App() {
                 Find Your Best Friend
               </Button>
             </div>
-            <div className="relative z-10 mx-auto mt-4 hidden max-w-2xl justify-center px-4 md:mt-6 md:flex">
-              <p className="typography-large text-center">
+            <div className="relative z-10 mx-auto mt-4 hidden max-w-2xl justify-center px-4 md:mt-6 md:flex md:max-w-xl">
+              <p className="typography-large text-center md:text-xl">
                 Left behind and waiting each day—many gentle hearts long for a
                 warm lap and a loving home. We are Autonomous Non-profit
                 Organization (ANO), we only spend and never earn. We rely solely
@@ -84,7 +84,7 @@ function App() {
             </div>
             <SocialLinks
               size="sm"
-              className="xs:mt-10 tablet-sm:mt-12 mt-8 gap-5 min-[425px]:mt-6"
+              className="xs:mt-10 tablet-sm:mt-12 mt-8 gap-5 min-[425px]:mt-6 md:mt-8"
               iconColor="text-neutral-50"
               bgColor="bg-primary-500"
             />
@@ -101,7 +101,7 @@ function App() {
                 bottom="0"
                 right="0"
                 zIndex={2}
-                className="tablet-sm:translate-x-[20%] tablet-sm:translate-y-1/4 translate-x-1/3 translate-y-1/2 min-[425px]:translate-x-1/5 min-[425px]:translate-y-1/3 md:bottom-24 lg:bottom-32"
+                className="tablet-sm:translate-x-[20%] tablet-sm:translate-y-1/4 translate-x-1/3 translate-y-1/2 min-[425px]:translate-x-1/5 min-[425px]:translate-y-1/3 lg:bottom-32"
               />
               <DecorativeCircle
                 size={42}
@@ -114,7 +114,7 @@ function App() {
                 bottom="0"
                 right="0"
                 zIndex={1}
-                className="tablet-sm:-translate-x-[50%] tablet-sm:translate-y-[35%] -translate-x-1/2 translate-y-1/2 min-[425px]:-translate-x-[60%] min-[425px]:translate-y-[40%] md:bottom-16 lg:bottom-22"
+                className="tablet-sm:-translate-x-[50%] tablet-sm:translate-y-[35%] -translate-x-1/2 translate-y-1/2 min-[425px]:-translate-x-[60%] min-[425px]:translate-y-[40%] md:-mr-8 lg:bottom-22"
               />
               <DecorativeCircle
                 size={20}
@@ -128,12 +128,12 @@ function App() {
                 bottom="0"
                 right="0"
                 zIndex={0}
-                className="tablet-sm:-translate-x-[240%] tablet-sm:translate-y-[35%] -translate-x-[250%] translate-y-1/2 min-[425px]:-translate-x-[200%] min-[425px]:translate-y-1/2 md:bottom-10 lg:bottom-14"
+                className="tablet-sm:-translate-x-[240%] tablet-sm:translate-y-[35%] -translate-x-[250%] translate-y-1/2 min-[425px]:-translate-x-[200%] min-[425px]:translate-y-1/2 md:-translate-x-[190%] lg:bottom-14"
               />
             </div>
             <img
               src={RottweilerImage}
-              className="xs:h-68 xs:w-68 tablet-sm:w-96 tablet-sm:h-96 absolute right-0 bottom-0 z-3 h-54 w-54 object-contain sm:h-70 sm:w-70 md:h-64 md:w-64 lg:h-80 lg:w-80"
+              className="xs:h-68 xs:w-68 tablet-sm:w-96 tablet-sm:h-96 absolute right-0 bottom-0 z-3 h-54 w-54 object-contain sm:h-70 sm:w-70 md:h-78 md:w-78 lg:h-80 lg:w-80"
               alt="Rottweiler Dog Illustration"
               aria-hidden="true"
             />
