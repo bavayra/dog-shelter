223370898c3ac013ceb451a362656e077b33799f
Alix Bell <x.bavayra@gmail.com>
2026-01-21 14:43:29 +0100

    style(adoptionRules - 480): increased font-size for title, text desc, rule card's text and icon. Increased btns text

--- src/components/RuleCard.tsx
+++ src/components/RuleCard.tsx
@@ -7,10 +7,10 @@ const RuleCard = ({ number, text }: RuleCardProps) => {
   return (
     <div className="xs:mx-6 mx-2 flex h-full justify-center rounded-lg bg-white px-6 py-4 text-center shadow-md sm:mx-8 md:mx-16 lg:mx-4">
       <div className="flex items-center gap-2">
-        <span className="text-primary-500 bg-primary-100 -ml-3 flex h-5 w-5 shrink-0 items-center justify-center rounded-full font-bold lg:h-8 lg:w-8">
+        <span className="text-primary-500 bg-primary-100 tablet-sm:h-8 tablet-sm:w-8 -ml-3 flex h-5 w-5 shrink-0 items-center justify-center rounded-full font-bold">
           {number}
         </span>
-        <p className="typography-small xs:text-base text-sm font-normal md:text-xl lg:px-2 lg:text-lg">
+        <p className="typography-small xs:text-base tablet-sm:text-lg text-sm font-normal md:text-xl lg:px-2 lg:text-lg">
           {text}
         </p>
       </div>
--- src/features/AdoptionRules.tsx
+++ src/features/AdoptionRules.tsx
@@ -15,11 +15,11 @@ const AdoptionRules = () => {
   return (
     <section id="adoption-rules" className="relative z-1 bg-neutral-50 py-6">
       <div className="container mx-auto px-4 sm:mt-4">
-        <h2 className="typography-h2 xs:text-4xl mb-0 text-center md:text-5xl">
+        <h2 className="typography-h2 xs:text-4xl tablet-sm:text-5xl mb-0 text-center">
           Adoption Rules
         </h2>
 
-        <p className="typography-body xs:text-lg mt-4 mb-6 text-center sm:text-lg md:px-16 md:text-2xl md:tracking-tight">
+        <p className="typography-body xs:text-lg tablet-sm:text-xl mt-4 mb-6 text-center sm:text-lg md:px-16 md:text-2xl md:tracking-tight">
           Please read our adoption guidelines carefully to ensure the best match
           for both you and your future companion.
         </p>
@@ -36,7 +36,7 @@ const AdoptionRules = () => {
           <a
             href="https://vk.com/topic-215318612_49115975"
             rel="noopener noreferrer"
-            className="bg-primary-500 hover:text-primary-200 hover:bg-primary-700 text-primary-50 xs:text-base mb-4 inline-block rounded-full px-5 py-3 text-xs font-semibold transition-transform hover:scale-105 md:mt-3 md:mb-6"
+            className="bg-primary-500 hover:text-primary-200 hover:bg-primary-700 tablet-sm:text-lg text-primary-50 xs:text-base mb-4 inline-block rounded-full px-5 py-3 text-xs font-semibold transition-transform hover:scale-105 md:mt-3 md:mb-6"
           >
             See All The Rules
           </a>
@@ -45,7 +45,7 @@ const AdoptionRules = () => {
           <a
             href="https://vk.com/topic-215318612_49115985"
             rel="noopener noreferrer"
-            className="bg-primary-500 hover:text-primary-200 hover:bg-primary-700 text-primary-50 xs:text-base inline-block rounded-full px-5 py-3 text-xs font-semibold transition-transform hover:scale-105"
+            className="bg-primary-500 hover:text-primary-200 tablet-sm:text-lg hover:bg-primary-700 text-primary-50 xs:text-base inline-block rounded-full px-5 py-3 text-xs font-semibold transition-transform hover:scale-105"
           >
             See The Contract Template
           </a>
