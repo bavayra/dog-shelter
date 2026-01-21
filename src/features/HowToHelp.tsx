fc87bd52799be5a1420d55b3dafb46386b4514a8
Alix Bell <x.bavayra@gmail.com>
2026-01-21 14:30:48 +0100

    style(howToHelp-480): increased font sizes for the header and text blocks, increased puppy's image's w, corrected px for the text

--- src/features/HowToHelp.tsx
+++ src/features/HowToHelp.tsx
@@ -5,14 +5,14 @@ const HowToHelp = () => {
   return (
     <section
       id="help"
-      className="bg-primary-50 border-b-primary-500 relative z-1 min-h-[75vh] scroll-mt-20 overflow-hidden border-b-3 pt-6 pb-30 sm:pt-10 lg:min-h-[90vh] xl:min-h-[80vh]"
+      className="bg-primary-50 border-b-primary-500 tablet-sm:min-h-[80vh] relative z-1 min-h-[75vh] scroll-mt-20 overflow-hidden border-b-3 pt-6 pb-30 sm:pt-10 lg:min-h-[90vh] xl:min-h-[80vh]"
     >
       <div className="z-10 container mx-auto px-4">
-        <h2 className="typography-h2 xs:text-4xl mb-6 text-center md:mt-6 md:text-5xl xl:text-7xl">
+        <h2 className="typography-h2 xs:text-4xl tablet-sm:text-5xl tablet-sm:mt-0 mb-6 text-center xl:text-7xl">
           How to Help ROTVODOM
         </h2>
-        <div className="mx-auto mb-6 max-w-3xl text-center md:mb-6 md:px-12">
-          <p className="typography-body xs:text-lg mb-2 leading-relaxed md:text-2xl">
+        <div className="tablet-sm:px-4 mx-auto mb-6 max-w-3xl text-center md:mb-6 md:px-12">
+          <p className="typography-body xs:text-lg tablet-sm:text-2xl mb-2 leading-relaxed">
             Our shelter relies{' '}
             <span className="text-primary-500 font-semibold">entirely</span> on
             the kindness and generosity of people like{' '}
@@ -20,7 +20,7 @@ const HowToHelp = () => {
             donation helps us provide food, medical care, and a safe home for
             dogs in need.
           </p>
-          <p className="typography-body xs:text-lg leading-relaxed md:text-2xl">
+          <p className="typography-body xs:text-lg tablet-sm:text-2xl leading-relaxed">
             Your support makes a{' '}
             <span className="text-primary-500 font-semibold">
               real difference
@@ -52,7 +52,7 @@ const HowToHelp = () => {
         src={RottweilerPupImage}
         alt=""
         aria-hidden="true"
-        className="from-primary-50 pointer-events-none absolute bottom-0 left-1/2 z-3 h-auto w-full max-w-2xl -translate-x-1/2 translate-y-1/4 overflow-visible bg-linear-to-b to-neutral-50 object-contain sm:w-110 md:w-200 md:max-w-3xl lg:w-130 lg:bg-none xl:w-154"
+        className="from-primary-50 tablet-sm:w-120 pointer-events-none absolute bottom-0 left-1/2 z-3 h-auto w-full max-w-2xl -translate-x-1/2 translate-y-1/4 overflow-visible bg-linear-to-b to-neutral-50 object-contain sm:w-110 md:w-200 md:max-w-3xl lg:w-130 lg:bg-none xl:w-154"
       />
     </section>
   );
