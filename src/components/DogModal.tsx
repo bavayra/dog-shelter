77dd13e881587ae19c45168b459096e007a2996b
Alix Bell <x.bavayra@gmail.com>
2026-01-22 18:03:28 +0100

    style(dogModal-1024): removed font weight and increased font size for the description

--- src/components/DogModal.tsx
+++ src/components/DogModal.tsx
@@ -187,7 +187,7 @@ const DogModal = ({ dog, isOpen, onClose }: DogModalProps) => {
 
           <div className="mb-4">
             <div
-              className="xs:text-lg px-2 text-justify text-sm leading-tight font-semibold text-neutral-700"
+              className="xs:text-lg px-2 text-justify text-sm leading-tight font-semibold text-neutral-700 lg:text-xl lg:font-normal"
               dangerouslySetInnerHTML={{ __html: sanitized }}
             />
           </div>
