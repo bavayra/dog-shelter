aa71532baf522a5d448bcede75c73dc19e1ed9dc
Alix Bell <x.bavayra@gmail.com>
2026-01-24 12:39:33 +0100

    style(howToHelp-1280): increased h of the section to min-h-screen

--- src/features/HowToHelp.tsx
+++ src/features/HowToHelp.tsx
@@ -5,7 +5,7 @@ const HowToHelp = () => {
   return (
     <section
       id="help"
-      className="bg-primary-50 border-b-primary-500 tablet-sm:min-h-[80vh] tablet-lg:min-h-[60vh] relative z-1 min-h-[75vh] scroll-mt-20 overflow-hidden border-b-3 pt-6 pb-30 sm:pt-10 md:min-h-[72vh] lg:min-h-[85vh] xl:min-h-[80vh]"
+      className="bg-primary-50 border-b-primary-500 tablet-sm:min-h-[80vh] tablet-lg:min-h-[60vh] relative z-1 min-h-[75vh] scroll-mt-20 overflow-hidden border-b-3 pt-6 pb-30 sm:pt-10 md:min-h-[72vh] lg:min-h-[85vh] xl:min-h-screen"
     >
       <div className="z-10 container mx-auto px-4">
         <h2 className="typography-h2 xs:text-4xl tablet-sm:text-5xl tablet-sm:mt-0 mb-6 text-center lg:text-7xl">
