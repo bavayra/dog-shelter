f9970fcbfeda274c305bc155ea723827a2e4564b
Alix Bell <x.bavayra@gmail.com>
2026-01-31 15:33:14 +0100

    style(footer-1280): increased gap between blocks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ import ContactInfo from './ContactInfo';
 const Footer = () => {
   return (
     <footer className="bg-primary-900 py-6 text-neutral-50">
-      <div className="tablet-lg:grid-cols-2 4xl:gap-80 tablet-lg:gap-40 container mx-auto mb-6 grid grid-cols-1 justify-items-center gap-6 px-4 md:mb-2 md:w-170 md:grid-cols-3 md:gap-5 lg:gap-72">
+      <div className="tablet-lg:grid-cols-2 4xl:gap-80 tablet-lg:gap-40 container mx-auto mb-6 grid grid-cols-1 justify-items-center gap-6 px-4 md:mb-2 md:w-170 md:grid-cols-3 md:gap-5 lg:gap-72 xl:gap-120">
         <div>
           <h3 className="xs:text-base tablet-lg:text-lg 4xl:text-2xl 5xl:mb-4 mb-2 text-center text-xs font-bold">
             {APP_NAME}
