acc182595e0b2445f72a6a4ad32e3a7526fa421f
Alix Bell <x.bavayra@gmail.com>
2026-02-02 19:20:28 +0100

    style(howToHelp-1280): decreased h of the section. Increased size of the decor circle and positioned it. Increased w of the text block.

--- src/App.tsx
+++ src/App.tsx
@@ -167,6 +167,7 @@ function App() {
                 size={64}
                 tabletLgSize={80}
                 lgSize={88}
+                xlSize={96}
                 xl2Size={120}
                 xl4Size={150}
                 color="primary-500"
@@ -174,7 +175,7 @@ function App() {
                 left="-30"
                 opacity={20}
                 zIndex={2}
-                className="tablet-lg:mt-36 tablet-lg:-ml-14 4xl:-ml-40 lg:mt-50 2xl:-ml-30"
+                className="tablet-lg:mt-36 tablet-lg:-ml-14 4xl:-ml-40 lg:mt-50 xl:mt-36 2xl:-ml-30"
               />
             </div>
             <HowToHelp />
