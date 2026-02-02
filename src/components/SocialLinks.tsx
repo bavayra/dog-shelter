7fc5abaf480cc0605e3cac759b8a70867715ef89
Alix Bell <x.bavayra@gmail.com>
2026-02-02 14:15:48 +0100

    refactor(footer-360): deleted duplicated classes and adjusted icon sizes

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -28,7 +28,7 @@ const SocialLinks = ({
 
   const sizeClasses = {
     sm: 'w-10 h-10  xs:w-14 xs:h-14 sm:w-16 sm:h-16 tablet-lg:w-20 tablet-lg:h-20 4xl:w-24 4xl:h-24 5xl:w-30 5xl:h-30',
-    md: 'w-12  h-12 xs:w-14 xs:h-14 md:w-10 md:h-10 4xl:w-16 4xl:h-16',
+    md: 'w-12  h-12  md:w-10 md:h-10 4xl:w-16 4xl:h-16',
   };
 
   const iconSizeMap = {
@@ -52,7 +52,7 @@ const SocialLinks = ({
           <Icon
             as={social.icon}
             size={iconSizeMap[size]}
-            className={`${iconColor} xs:w-8 xs:h-8 5xl:w-16 5xl:h-16 tablet-lg:w-10 tablet-lg:h-10 4xl:w-14 4xl:h-14 border-primary-200 rounded-full border sm:h-10 sm:w-10 md:h-6 md:w-6`}
+            className={`${iconColor} xs:w-12 xs:h-12 5xl:w-16 5xl:h-16 tablet-lg:w-10 tablet-lg:h-10 4xl:w-14 4xl:h-14 border-primary-200 rounded-full border sm:h-10 sm:w-10 md:h-6 md:w-6`}
             aria-hidden="true"
           />
         </a>
