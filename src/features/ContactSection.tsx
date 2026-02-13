dca26b416d0aac58a00d5474ee8e6d534b86dd27
Alix Bell <x.bavayra@gmail.com>
2026-02-13 17:52:12 +0100

    style(contactUs-834): repositioned decor circle

--- src/features/ContactSection.tsx
+++ src/features/ContactSection.tsx
@@ -121,7 +121,7 @@ const ContactSection = () => {
           right="0"
           zIndex={10}
           opacity={70}
-          className="xs:-translate-x-[180%] xs:translate-y-1/5 tablet-sm:-translate-x-[240%] tablet-lg:-translate-x-[300%] tablet-lg:translate-y-[70%] 3xl:-translate-x-[700%] 4xl:mt-0 5xl:-translate-x-[725%] 5xl:-mt-1 4xl:mr-20 translate-x-[200%] sm:mr-6 md:-translate-x-[355%] lg:mt-0 lg:-translate-x-[700%] xl:-mt-3 xl:-translate-x-[695%] 2xl:-translate-x-[860%] [@media(min-height:1152px)]:mr-32"
+          className="xs:-translate-x-[180%] xs:translate-y-1/5 tablet-sm:-translate-x-[240%] tablet-lg:-translate-x-[280%] tablet-lg:translate-y-[70%] 3xl:-translate-x-[700%] 4xl:mt-0 5xl:-translate-x-[725%] 5xl:-mt-1 4xl:mr-20 translate-x-[200%] sm:mr-6 md:-translate-x-[355%] lg:mt-0 lg:-translate-x-[700%] xl:-mt-3 xl:-translate-x-[695%] 2xl:-translate-x-[860%] [@media(min-height:1152px)]:mr-32"
         />
       </div>
       <h2 className="typography-h2 xs:text-5xl 4xl:text-9xl tablet-lg:text-8xl xs:mb-8 relative z-11 mb-4 text-center md:text-7xl">
